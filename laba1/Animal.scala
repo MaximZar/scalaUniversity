19d711695b53c12478a2343ce1d394f110565a40
Maxim Zadorkin <m.a.zadorkin@gmail.com>
2020-04-29 21:23:40 +0300

    Done Laba1: Classes.scala - Animal.scala

--- laba1/Animal.scala
+++ laba1/Animal.scala
@@ -7,3 +7,34 @@ object Animal {
   "parrot" -> Array[String]("bird", "vegetables"),
   "goldfish" -> Array[String]("fish", "plants"))
 }
+
+//2 часть задания (начиная с пункта d)
+trait Animal {
+  case object mammal extends Animal
+  case object bird extends Animal
+  case object fish extends Animal
+}
+object Animal {
+  def knownAnimal(name: String): Boolean = {
+    name match {
+      case "cat" => true
+      case "parrot" => true
+      case "goldfish" => true
+      case _ => false
+    }
+  }
+  def apply(name: String): Option[Animal] = Some.apply(Animal.this)
+}
+class Animal(name: String, food: String) {
+  trait Food {
+    case class Meat
+    case class Vegetables
+    case class Plants
+  }
+  object Food {
+    def apply(food: String): Option[Food] = Some.apply(Food.this)
+  }
+}
+
+
+
