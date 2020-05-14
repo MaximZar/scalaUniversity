67cfd6f72f16c6cfcc755dd9eb05743893c78b6a
Maxim Zadorkin <m.a.zadorkin@gmail.com>
2020-05-14 19:32:58 +0300

    update 3

--- laba3/Maps.scala
+++ laba3/Maps.scala
@@ -1,9 +1,7 @@
-
-
 /** Напишите вашу реализацию в тестовые функции.
-  * 
-  * https://docs.scala-lang.org/overviews/collections/maps.html
-  */
+ *
+ * https://docs.scala-lang.org/overviews/collections/maps.html
+ */
 object Maps {
 
   case class User(name: String, age: Int)
@@ -11,15 +9,33 @@ object Maps {
   /* a) В данной Seq[User] сгруппируйте пользователей по имени (`groupBy`) и вычислите средний возраст: `name -> averageAge`
    *    Вы можете реализовать ваше решение в теле тестовой функции. Не изменяйте сигнатуру.
    */
-  def testGroupUsers(users: Seq[User]): Map[String, Int] = Map.empty
+  def testGroupUsers(users: Seq[User]): Map[String, Int] = {
+    var map: Map[String, Int] = Map()
+    var name = users.groupBy(_.name)
+    for (e <- name) {
+      var average = e._2.toBuffer.foldLeft[Int](0)((a, next) => a + next.age) /
+      e._2.toBuffer.size
+      map += (e._1 -> average)
+    }
+    map
+  }
 
   /* b) Дана `Map[String, User]` состоящая из имен пользователей `User`, сколько имен пользователей, содержащихся в Map, содержат подстроку "Adam"?
    *    Вы можете реализовать ваше решение в теле тестовой функции. Не изменяйте сигнатуру.
    */
-  def testNumberFrodos(map: Map[String, User]): Int = 0
+  def testNumberFrodos(map: Map[String, User]): Int = {
+    var a: Int = 0
+    for (num <- map) {
+      if (num._2.name.indexOf("Adam") != (-1))
+        a += 1
+    }
+    a
+  }
 
   /* c) Удалите всех пользователей возраст которых менее 35 лет.
    *    Вы можете реализовать ваше решение в теле тестовой функции. Не изменяйте сигнатуру.
    */
-  def testUnderaged(map: Map[String, User]): Map[String, User] = map
+  def testUnderaged(map: Map[String, User]): Map[String, User] = {
+    map.filter(k => k._2.age >= 35)
+  }
 }
